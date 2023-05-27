684b98a608be65a069c73fe3ba7730c925e0d9a9
Cornelius Renken <cornelius.renken@gmail.com>
2023-05-27 12:39:00 -0700

    temporary front end

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<form method="POST">
+  <label>
+    Query
+    <input name="query" type="text" />
+  </label>
+</form>
+
+<style>
+  :global(body) {
+    margin: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: min(100vh, 500px);
+  }
+
+  label {
+    width: 50vw;
+    display: flex;
+    gap: 1rem;
+  }
+
+  input {
+    flex: 1;
+  }
+</style>
