101ab768b2912d2e6556d5d9f8c5f78b5ddd2784
Cornelius Renken <cornelius.renken@gmail.com>
2023-05-31 17:55:14 -0700

    added pinecone

---
+++ src/lib/server/utils/pinecone/index.ts
@@ -0,0 +1,11 @@
+import { PineconeClient } from '@pinecone-database/pinecone';
+import { PINECONE_ENVIRONMENT, PINECONE_SECRET } from '$env/static/private';
+
+const pinecone = new PineconeClient();
+
+await pinecone.init({
+  environment: PINECONE_ENVIRONMENT,
+  apiKey: PINECONE_SECRET,
+});
+
+export default pinecone;
